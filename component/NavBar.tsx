6f679f3bf4f582351fa84647f1cda47c4d8d8218
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-06-16 20:59:10 +0900

    add
    스타일 공부중

--- component/NavBar.tsx
+++ component/NavBar.tsx
@@ -1,31 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        // style={{ color: router.pathname === "/" ? "red" : "blue" }}
-        href="/"
-      >
+      <Link className="hello" href="/">
         Home
       </Link>
-      <Link
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join("")}
-        // style={{ color: router.pathname === "/about" ? "red" : "blue" }}
-        href="/about"
-      >
-        About
-      </Link>
+      <Link href="/about">About</Link>
+      <style jsx>
+        {`
+          nav {
+            background-color: tomato;
+          }
+          hello {
+            text-decoration: none;
+          }
+        `}
+      </style>
     </nav>
   );
 }
