c1ffa17c6baa506ecc7caed1aa8b31ebc88a6652
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-06-16 15:48:02 +0900

    simple test1
    카운터 예제를 해보았다
    미리 렌더링 도딘다는 것은 어썸한것을 맛볼수 있었다
    JS를 비활성화해도 초기 상태를 볼수있는 좋은것

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
 export default function Home() {
-  return "hisz";
+  const [cnt, setCnt] = useState(0);
+  return (
+    <div>
+      <h1>hello {cnt}</h1>
+      <button onClick={() => setCnt((pre) => pre + 1)}>+</button>
+    </div>
+  );
 }
