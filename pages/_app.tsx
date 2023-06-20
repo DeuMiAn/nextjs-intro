a8e53da507eec50ad6d2935a4575e00b099d3b7e
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-06-21 00:10:15 +0900

    getData(fetch and reactQuery
    api를 통한 데이터 가져오기와 간단한 로딩스테이트를 구현함

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "../component/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const client = new QueryClient();
   return (
-    <Layout>
-      <Component {...pageProps}></Component>
-      <span>hello</span>
-      {/* 킹치만 _App에서 선언한 글로벌이라면 말은 달라지겠지 */}
-      <style global jsx>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
-    </Layout>
+    <QueryClientProvider client={client}>
+      <Layout>
+        <Component {...pageProps}></Component>
+        <span>hello</span>
+      </Layout>
+    </QueryClientProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,47 @@
 import Head from "next/head";
 import Seo from "../component/Seo";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
+const API_KEY = "7e3db6a6ed2925c60e07724d7a2042bb";
+
+function getMovies() {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  ).then((response) => response.json());
+}
 
 export default function Home() {
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (
+  //       await fetch(
+  //         `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+  //         {
+  //           method: "GET",
+  //           // headers: {
+  //           //   Authorization: ` Bearer ${API_KEY}`,
+  //           //   accept: "application/json",
+  //           // },
+  //         }
+  //       )
+  //     ).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
+  const { data, isLoading } = useQuery(["movies", "popular"], getMovies);
+
   return (
     <div>
       <Seo title={"Home"} />
-      <h1>hello</h1>
-      {/* global은 같은페이지 네에서만 적용되는것  */}
-      <style global jsx>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
+      {/* <h1>hello</h1> */}
+      {isLoading && <h4>Loading...</h4>}
+      {data?.results.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
